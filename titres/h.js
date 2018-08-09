bdfa14422dd006df9935e3fd010b7e48e11e8a47
Francois Romain <francoisromain@gmail.com>
2018-08-09 13:27:41 +0200

    fix: supprime la première lettre de la réf si ne commence pas par C

--- titres/h.js
+++ titres/h.js
@@ -1,7 +1,7 @@
 const titresSpreadsheetToJson = require('./_titres-spreadsheet-to-json')
 const compare = require('./_compare.js')
 const type = 'h'
-const dbSpreadsheetId = '1Jn-iWWY12MSY2ypFILIBtqgZGS4gAECyHbkUxxH_O0Y'
+const dbSpreadsheetId = '1ROInBEn06q1zvsw88kUFvtsz4yyXsHnF8hDSSPNoYw0'
 const jorfSpreadsheetId = '1h_tqp4i9hByh1KtdFmAsGephlH8Sg4phpoKbm2T97XU'
 
 module.exports = async () => {
